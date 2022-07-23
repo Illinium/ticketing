63a745cf37fa6d9c4d5f464cb7ee75110c95252d
unknown <khodorko88@gmail.com>
2022-07-23 22:57:20 +0300

    edit some tests

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,22 +1,23 @@
-import request from "supertest";
-import { app } from "../../app";
-import { authHelper } from "../../test/auth-helper";
+import request from 'supertest';
+import { app } from '../../app';
+import { authHelper } from '../../test/auth-helper';
 
-it("responds with details about current user", async () => {
+it('responds with details about current user', async () => {
     const cookie = await authHelper();
 
     const resp = await request(app)
         .get('/api/users/currentuser')
         .set('Cookie', cookie)
-        .send().expect(200);
+        .send()
+        .expect(200);
 
-        expect(resp.body.currentUser.email).toEqual("test@test.com");
+    expect(resp.body.currentUser.email).toEqual('test@test.com');
 });
 
-it("you are not autorized", async () => {
+it('you are not autorized', async () => {
     const response = await request(app)
         .get('/api/users/currentuser')
-        .send().expect(401);
-    // console.log(response.body.errors[0].message);
-    expect(response.body.errors[0].message).toEqual("You are not autorized!");
+        .send()
+        .expect(401);
+    expect(response.body.errors[0].message).toEqual('You are not autorized!');
 });
--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -1,19 +1,23 @@
-import request from "supertest";
-import { app } from "../../app";
+import request from 'supertest';
+import { app } from '../../app';
 
-it("clears a cookie after signing out", async () => {
+it('clears a cookie after signing out', async () => {
     await request(app)
         .post('/api/users/signup')
         .send({
-            email: "test@test.com",
-            password: "password"
-        }).expect(201);
+            email: 'test@test.com',
+            password: 'password',
+        })
+        .expect(201);
 
     const responce = await request(app)
         .post('/api/users/signout')
-        .send().expect(200);
+        .send()
+        .expect(200);
 
-        // console.log(responce.get('Set-Cookie')[0]);
+    // console.log(responce.get('Set-Cookie')[0]);
 
-        expect(responce.get('Set-Cookie')[0]).toEqual('session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly');
+    expect(responce.get('Set-Cookie')[0]).toEqual(
+        'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
+    );
 });
